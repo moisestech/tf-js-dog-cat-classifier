bf9402e9e4396226b5c142f1ec3f0896409f3a41
moisestech <m@moises.tech>
2021-02-07 10:39:35 -0500

    entry, output, mode, modules rules

--- webpack.config.js
+++ webpack.config.js
@@ -2,5 +2,37 @@ const path = require('path');
 
 module.exports = {
   entry: './src/index.js',
-  path: path.resolve(__dirname, 'dist')
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  mode: 'development',
+  modules: {
+    rules: [
+      {
+        test:/\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test:/\.mp4/,
+        loader: 'file-loader',
+        options: {
+          name:'[name].[ext]',
+          outputPath:'video/'
+        }
+      },
+      {
+        test:/\.(jpg|jpeg|png|gif)$/,
+        loader:'file-loader',
+        options: {
+          name:'[name].[ext]',
+          outPath:'image/'
+        }
+      },
+      {
+        test:/\.ico$/,
+        loader:'file-loader'
+      }
+    ]
+  }
 }
