1807130d6eb1655c93fd8eda19fb556072ed6cad
moisestech <m@moises.tech>
2021-02-08 06:53:11 -0500

    module babel-loader plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,7 @@
 const path = require('path');
+const cleanWebpackPlugin = require('clean-webpack-plugin');
+const htmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
   entry: './src/index.js',
@@ -7,8 +10,27 @@ module.exports = {
     path: path.resolve(__dirname, 'dist')
   },
   mode: 'development',
+  devServer: {
+    contentBase:'./dist',
+    port: 3030,
+    hot: true
+  },
   modules: {
     rules: [
+      {
+        test:/\.js$/,
+        exclude:/node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets:['@babel/preset-env'],
+            plugins: [
+              '@babel/plugin-transform-runtime',
+              ['@babel/plugin-proposal-pipeline-oprator', {proposal:'minimal'}]
+            ]
+          }
+        }
+      },
       {
         test:/\.css$/,
         use: ['style-loader', 'css-loader']
@@ -34,5 +56,14 @@ module.exports = {
         loader:'file-loader'
       }
     ]
-  }
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new cleanWebpackPlugin(['./dist']),
+    new htmlWebpackPlugin({
+      filename: 'index.html',
+      template: './template.html',
+      favicon: './favicon.ico'
+    })
+  ]
 }
