793fe9802c20ca2a36ff406960153a032e1ff30b
moisestech <m@moises.tech>
2021-02-13 14:05:30 -0500

    init react react-dom styles.css

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
+import "./styles.css";
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+
 let project_name = "Tensorflow.js React Dog Cat Classifier";
 
 console.log(project_name);
