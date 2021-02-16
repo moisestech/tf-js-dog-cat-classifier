84666249f2327976901c40c8e978d682f3784cd8
moisestech <m@moises.tech>
2021-02-16 06:56:01 -0500

    imp React, exp func comp App

---
+++ src/App/index.js
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+
+export default function App() {
+  let project_name = "Tensorflow.js React Dog Cat Classifier";
+
+  return (  
+    {console.log(project_name)}
+  )
+}
+
+// https://levelup.gitconnected.com/build-ad-dog-classifier-with-react-and-tensorflow-js-in-minutes-f08e98608a65
+// https://www.youtube.com/watch?v=nxAsWjSc-94
