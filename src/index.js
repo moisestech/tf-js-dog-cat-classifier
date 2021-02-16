f77587a0bcad6119e021d73fbecadc1f35b4f489
moisestech <m@moises.tech>
2021-02-16 06:55:03 -0500

    imp react react-dom styles, imp func comp App

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
-import "./styles.css";
-import React, { useState } from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles.css';
+import App from './App';
 
-let project_name = "Tensorflow.js React Dog Cat Classifier";
-
-console.log(project_name);
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
