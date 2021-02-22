9667f20f40fe1d3e2adc8b73aadecadcdc3581b1
moisestech <m@moises.tech>
2021-02-22 06:17:47 -0500

    importing state machine object in App for reference

--- src/App/index.js
+++ src/App/index.js
@@ -12,3 +12,15 @@ export default function App({project_name = "Tensorflow.js React Dog Cat Classif
 // video: https://www.youtube.com/watch?v=nxAsWjSc-94
 // blog: https://levelup.gitconnected.com/build-ad-dog-classifier-with-react-and-tensorflow-js-in-minutes-f08e98608a65
 // code: https://github.com/jonnyk20/dogscope-react
+
+const stateMachine = {
+  initial: "initial",
+  states: {
+  initial: { on: { next: "loadingModel", text: "Load Model" } },
+  loadingModel: { on: { next: "modelReady", text: "Loading Model" } },
+  modelReady: { on: { next: "imageReady", text: "Upload Image"} },
+  imageReady: { on: { next: "identifying" }, text: "Identify Breed", showImage: true },
+  identifying: { on: { next: "complete", text: "Identifying…"} },
+  complete: { on: { next: "modelReady" }, text: "Reset", showImage: true, showResults: true }
+  }
+ };
