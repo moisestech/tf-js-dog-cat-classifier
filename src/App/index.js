ffd72ae29e0f3b61dfac3cddc0395d287546f7ef
moisestech <m@moises.tech>
2021-02-25 05:40:13 -0500

    moved stateMachine to top of file after imports

--- src/App/index.js
+++ src/App/index.js
@@ -1,6 +1,21 @@
 import React, { useState, useReducer } from "react";
 
+// model to detect inputted camera images
 import mobilenet from '@tensorflow-models/mobilenet';
+
+// manage the state of the app
+const stateMachine = {
+    initial: "initial",
+    states: {
+    initial: { on: { next: "loadingModel", text: "Load Model" } },
+    loadingModel: { on: { next: "modelReady", text: "Loading Model" } },
+    modelReady: { on: { next: "imageReady", text: "Upload Image"} },
+    imageReady: { on: { next: "identifying" }, text: "Identify Breed", showImage: true },
+    identifying: { on: { next: "complete", text: "Identifying…"} },
+    complete: { on: { next: "modelReady" }, text: "Reset", showImage: true, showResults: true }
+  }
+};
+
 
 export default function App({project_name = "Tensorflow.js React Dog Cat Classifier"}) {
   const [appState, dispatch] = useReducer(reducer, stateMachine.initial);
@@ -13,15 +28,3 @@ export default function App({project_name = "Tensorflow.js React Dog Cat Classif
 // video: https://www.youtube.com/watch?v=nxAsWjSc-94
 // blog: https://levelup.gitconnected.com/build-ad-dog-classifier-with-react-and-tensorflow-js-in-minutes-f08e98608a65
 // code: https://github.com/jonnyk20/dogscope-react
-
-const stateMachine = {
-  initial: "initial",
-  states: {
-  initial: { on: { next: "loadingModel", text: "Load Model" } },
-  loadingModel: { on: { next: "modelReady", text: "Loading Model" } },
-  modelReady: { on: { next: "imageReady", text: "Upload Image"} },
-  imageReady: { on: { next: "identifying" }, text: "Identify Breed", showImage: true },
-  identifying: { on: { next: "complete", text: "Identifying…"} },
-  complete: { on: { next: "modelReady" }, text: "Reset", showImage: true, showResults: true }
-  }
- };
